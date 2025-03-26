9580d21a5b5ad6c1ed5e79d6ffbc33ed16c6d45c
unknown <Anup@LAPTOP-4L0F33J3>
2025-03-26 19:11:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import todoRoutes from "./routes/todoRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use("/todos", todoRoutes);
+app.listen(5000, () => console.log("Server running on port 5000"));
